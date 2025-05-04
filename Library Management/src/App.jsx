c2774b239d1daa07f54de5c835c37f3f43398d93
Akhileshyadu00 <akhileshyadu001@gmail.com>
2025-05-04 22:06:49 +0530

    Books done

--- Library Management/src/App.jsx
+++ Library Management/src/App.jsx
@@ -2,14 +2,30 @@ import BookList from './components/BookList'
 import Book from './components/Book'
 import { Books } from './utils/mockData'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
   
+const [searchText, setSearchText] = useState("")
+const [filteredBooks, setFilteredBooks] = useState(Books)
+
+  function handleSearch(){
+    const filterBooks = Books.filter((book) => 
+      book.title.toLowerCase().includes(searchText.toLowerCase()));
+    setFilteredBooks(filterBooks)
+  }
 
   return (
     <>
     <h1>Library Management System</h1>
-    <BookList booksData = {Books}/>
+    <div>
+      <h3>Search Book Here</h3>
+    <div>
+      <input type="text" className='' onChange={(e) => setSearchText(e.target.value)}/>
+      <button onClick={handleSearch}>Search</button>
+    </div>
+    </div>
+    <BookList booksData = {filteredBooks}/>
     </>
   )
 }
