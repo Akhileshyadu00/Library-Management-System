3b6cd62339b21771daac46fb67ff4f289eb6a21a
Akhileshyadu00 <akhileshyadu001@gmail.com>
2025-05-09 13:43:50 +0530

    custom hooks

--- Library Management/src/components/BookList.jsx
+++ Library Management/src/components/BookList.jsx
@@ -3,25 +3,43 @@ import { Books } from "../utils/mockData";
 import "./Book.css";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import useFetch from "../utils/useFetch";
 
 function BookList() {
   const [searchText, setSearchText] = useState('');
   const [filteredBooks, setFilteredBooks] = useState(Books); // Default to all books
 
-  useEffect(() => {
-    fetchData();
-  }, [filteredBooks]);
+  // useEffect(() => {
+  //   fetchData();
+  // }, [filteredBooks]);
 
-  async function fetchData() {
-    try {
-      const response = await fetch("https://openlibrary.org/search.json?q=test");
-      const json = await response.json();
-      console.log("API result", json);
-     
-    } catch (error) {
-      console.error("Failed to fetch data", error);
+  const {data, error, loading} = useFetch(
+    "https://openlibrary.org/search.json?q=test"
+  );
+
+  useEffect(() => {
+    if(data) {
+      setFilteredBooks(data)
     }
+  },[data])
+
+  if(error) {
+    return <p>Error in Loading: {error}</p>
   }
+  if(loading) {
+    return <p>Loading: {loading}</p>
+  }
+
+  // async function fetchData() {
+  //   try {
+  //     const response = await fetch("https://openlibrary.org/search.json?q=test");
+  //     const json = await response.json();
+  //     console.log("API result", json);
+     
+  //   } catch (error) {
+  //     console.error("Failed to fetch data", error);
+  //   }
+  // }
 
   const handleSearch = () => {
     const results = Books.filter(book =>
