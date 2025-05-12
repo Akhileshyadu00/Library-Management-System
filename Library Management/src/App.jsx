f9e472b72f870ba87fcf3ba0921f70d44e6a6a67
Akhileshyadu00 <akhileshyadu001@gmail.com>
2025-05-12 21:09:22 +0530

    redux-toolkit

--- Library Management/src/App.jsx
+++ Library Management/src/App.jsx
@@ -3,17 +3,21 @@ import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
 import UserContext from './utils/userContext'
 import './App.css'
+import { Provider } from 'react-redux';
+import appStore from './utils/Appstore'
+
+
 
 function App() {
   const [userName, setUserName] = useState("Akhilesh Yadav");
 
   return (
-    <>
+    <Provider store={appStore}>
       <Header />
       <UserContext.Provider value={{ loggedInUser: userName, setUserName }}>
         <Outlet />
       </UserContext.Provider>
-    </>
+    </Provider>
   )
 }
 
